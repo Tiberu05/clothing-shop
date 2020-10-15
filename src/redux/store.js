e212b3f1f08198869d88c738fe303a3c3a66e947
Tiberu05 <jclauc@gmail.com>
2020-10-15 15:01:41 +0300

    Removed Redux Dev Tools to test the app on ios Safari

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { persistStore } from 'redux-persist';
 import ReduxThunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+//import { composeWithDevTools } from 'redux-devtools-extension';
 
 
 import reducers from './reducers';
@@ -12,7 +12,6 @@ export const store = createStore(
     reducers, 
     compose (
         applyMiddleware(...middlewares),
-        composeWithDevTools()
     )
 );
 
